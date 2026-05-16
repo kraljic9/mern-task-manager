8ebe745bfb17589171dbe188938f22fa6870317d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-16 15:36:44 +0200

    Added comments and router

---
+++ routes/task.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import Task from "../models/task,js";
+
+// Setup router
+
+const router = express.Router;
+
+// Get tasks
+
+// Get individual task
+
+// Create task
+
+// Edit task
+
+// Delete task
