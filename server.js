c4aa301915edbefb7dfc4b2939f8090c5fcbebe7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-16 12:30:30 +0200

    Connected user route

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import MongoDB from "./db.js";
+import userRouter from "./routes/user.js";
 
 MongoDB();
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello server");
