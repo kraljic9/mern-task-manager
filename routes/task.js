8e3d7d842a00fd01a1250a9110e75bf4044383f8
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-16 22:23:28 +0200

    Added post and get routes

--- routes/task.js
+++ routes/task.js
@@ -1,17 +1,49 @@
 import express from "express";
 import mongoose from "mongoose";
-import Task from "../models/task,js";
+import Task from "../models/task.js";
+import auth from "../middleware/auth.js";
 
 // Setup router
 
-const router = express.Router;
+const router = express.Router();
 
 // Get tasks
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Task.find();
+    res.status(200).json({ posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Server error`, err });
+  }
+});
 
 // Get individual task
 
 // Create task
 
+router.post("/", auth, async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    if (!title) {
+      res.status(400).json({ message: "Error invaild credidentals" });
+    }
+
+    const user = await Task.create({
+      title,
+      user: req.user,
+    });
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Server error`, err });
+  }
+});
 // Edit task
 
 // Delete task
+
+export default router;
