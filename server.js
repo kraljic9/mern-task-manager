4cec8ee249323aa79527917c412ea11c15f9e925
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-16 13:36:30 +0200

    fixed express js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ MongoDB();
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
