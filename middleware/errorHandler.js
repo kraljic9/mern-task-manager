c5f883bf666579c06c53f5b5f609e4f95e5ef4d7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-17 12:11:08 +0200

    Added error handler middlerWare

---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const message = err.message;
+
+  res.staus(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
