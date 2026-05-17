c657f53490ef7edd0261ae5ed4ec8f9535cd44b7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-17 18:53:03 +0200

    Added if statement to see if zod error accured

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,10 @@ export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   const message = err.message;
 
+  if (err.message === "ZodError") {
+    statusCode = 400;
+    message = err.errors.map((e) => e.message).join("");
+  }
   res.staus(statusCode).json({
     message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
