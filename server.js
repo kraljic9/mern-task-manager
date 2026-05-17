e3c18b705a26748dee8fe7b1117e0e1782587e61
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-05-17 12:54:51 +0200

    Added errorHandler to app.use

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import MongoDB from "./db.js";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 MongoDB();
 const PORT = process.env.PORT || 8000;
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/task", taskRouter);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Hello server");
